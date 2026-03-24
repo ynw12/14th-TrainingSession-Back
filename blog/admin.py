965f036f558b204a02d38142d995c4ec562f81e9
LeeYeonWoo <23010034@ewha.ac.kr>
2026-03-24 20:00:44 +0900

    1NMN 관계

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment, Hashtag
 
 admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Hashtag)
 # Register your models here.
--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Hashtag(models.Model):
+    hashtag = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.hashtag
+    
 class Post(models.Model):
     title = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
     content = models.TextField(max_length=500)
+    hashtag = models.ManyToManyField(Hashtag)
 
     def __str__(self):
         return self.title
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    username = models.CharField(max_length=20)
+    comment_text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def approve(self):
+        self.save()
+    def __str__(self):
+        return self.comment_text
+
+    
