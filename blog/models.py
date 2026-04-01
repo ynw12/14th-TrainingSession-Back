d2a4c1cfd80866418a87f0dd48f5206d56676c23
LeeYeonWoo <23010034@ewha.ac.kr>
2026-04-02 07:43:48 +0900

    static&media

--- blog/models.py
+++ blog/models.py
@@ -1,21 +1,24 @@
 from django.db import models
 
-# Create your models here.
+# 해시태그 모델
 class Hashtag(models.Model):
     hashtag = models.CharField(max_length=100)
 
     def __str__(self):
         return self.hashtag
-    
+
+# 게시물 모델  
 class Post(models.Model):
     title = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
     content = models.TextField(max_length=500)
     hashtag = models.ManyToManyField(Hashtag)
+    photo = models.ImageField(blank=True, null=True, upload_to="post_photo")
 
     def __str__(self):
         return self.title
-    
+
+# 댓글 모델   
 class Comment(models.Model):
     post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
     username = models.CharField(max_length=20)
@@ -26,5 +29,4 @@ class Comment(models.Model):
         self.save()
     def __str__(self):
         return self.comment_text
-
     
